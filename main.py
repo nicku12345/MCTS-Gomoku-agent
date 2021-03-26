b4f6543f7017c9e5948dc705e104ba8ffff69129
nicku12345 <68750811+nicku12345@users.noreply.github.com>
2021-03-27 00:06:54 +0800

    testing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gomoku.game import Game
 from mct.mct import Node
 from mct.cache import Cache
 
-def run_episode(cache):
+def run_episode(cache, total = None, generation = 0):
     game = Game()
     while not game.is_ended:
         root = Node(game=game)
@@ -11,15 +11,21 @@ def run_episode(cache):
         i,j = root.best_action_idx
         game = game.take_action(i,j)
         game.display()
-        print(cache.memory_size)
+        print(f"Cache memory size: {cache.memory_size}")
+        print(f"Total game: {total}")
     cache.save_completed_game()
 
 if __name__ == "__main__":
     cache = Cache()
+    total = 0
+    generation = 0
     while True:
         num_game = 0
-        while num_game < 1000:
-            run_episode(cache=cache)
+        while num_game < 1:
+            run_episode(cache=cache, total = total, generation = generation)
             num_game += 1
+            total += 1
         Node.model.optimize(cache.memory)
         Node.model.save()
+        Node.model.reset()
+        generation += 1
