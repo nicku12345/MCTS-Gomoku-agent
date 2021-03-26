2e7682fae23a351b579137f9c1566274d6e28286
nicku12345 <68750811+nicku12345@users.noreply.github.com>
2021-03-27 00:14:18 +0800

    test

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from gomoku.game import Game
 from mct.mct import Node
 from mct.cache import Cache
 
+NUM_ITER = 300
+TRAIN_EVERY = 1
+MAX_ROUND = 30
+
 def run_episode(cache, total = None, generation = 0):
     game = Game()
-    while not game.is_ended:
+    round = 0
+    while not game.is_ended and round < MAX_ROUND:
         root = Node(game=game)
-        root.repeatedly_iterate(num_iter=300)
+        root.repeatedly_iterate(num_iter=NUM_ITER)
         cache.save_state(root=root)
         i,j = root.best_action_idx
         game = game.take_action(i,j)
         game.display()
-        print(f"Cache memory size: {cache.memory_size}")
+        #print(f"Cache memory size: {cache.memory_size}")
         print(f"Total game: {total}")
+        print(f"Generation" {generation})
+        round += 1
     cache.save_completed_game()
 
 if __name__ == "__main__":
@@ -21,11 +28,11 @@ if __name__ == "__main__":
     generation = 0
     while True:
         num_game = 0
-        while num_game < 1:
+        while num_game < TRAIN_EVERY:
             run_episode(cache=cache, total = total, generation = generation)
             num_game += 1
             total += 1
         Node.model.optimize(cache.memory)
         Node.model.save()
-        Node.model.reset()
+        cache.reset()
         generation += 1
